16d2088b5bda81acb4494bb422854e2452073878
RinaAraki <arakipomu1206@gmail.com>
2021-12-28 08:14:55 +0000

    [update]3comment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @showbook = Book.find(params[:id])
     @user = @showbook.user
     @book = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
