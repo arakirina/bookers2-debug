221c4920a6908cbee44855d1c79841aab2637dfa
RinaAraki <arakipomu1206@gmail.com>
2021-12-28 03:49:56 +0000

    [update]3favorite

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
   devise_for :users
+  resources :books do
+    resource :favorites, only: [:create,:destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+
 end
