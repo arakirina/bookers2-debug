c9e904aaf69a4cc567944d15bd66fe3d9b7fe7b3
RinaAraki <arakipomu1206@gmail.com>
2021-12-29 01:47:56 +0000

    [start]4

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,11 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
+  
+  has_many :follower, class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+  has_many :reverse_of_relationships, class_name:"relationship", foreign_key: "followers_id", dependent: :destroy
+  
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
 end
